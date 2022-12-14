17a9ef3b3452cb18694e25116874d63968e24b78
Maria SJ <maria.som.jur@gmail.com>
2022-12-14 18:47:02 +0100

    finish css

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import pickle from '../images/pikle-rick.png';
 
 const NotFound = () => {
   return (
     <div className="error">
-      <p className="error__text">El personaje que buscas no existe</p>
+      <img className="error__img" src={pickle} alt="pickle rick" title="Pickle Rick"/>
+      <p className="error__text">Vaya... parece que el personaje que buscas no existe</p>
       <Link className="error__goBack" to="/">Volver</Link>
     </div>
     );
